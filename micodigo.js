7f8b600840067330e185a30e53f5d4b2ab5e759b
Your Name <you@example.com>
2024-05-19 17:59:40 -0300

    página

---
+++ micodigo.js
@@ -0,0 +1,16 @@
+
+let nombre = "Damian";
+let apellido = "Cogo";
+
+console.log(nombre + " " + apellido)
+
+let numeroa = 10;
+let numerob = 20;
+let suma = numeroa + numerob;
+let numeroc = suma + 100;
+
+console.log (suma)
+
+console.log (numeroc);
+
+
